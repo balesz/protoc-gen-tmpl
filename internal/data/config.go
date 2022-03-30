99d05e60a9e1a99c0a36a6c729ae070f371b2ca7
balesz <balint.bode@gmail.com>
2022-03-30 21:58:30 +0200

    Add Types to Config

--- internal/data/config.go
+++ internal/data/config.go
@@ -25,6 +25,29 @@ type Config struct {
 		Name string
 		Path string
 	}
+	Types struct {
+		Enum     string
+		Map      string
+		Message  string
+		Repeated string
+		Scalar   struct {
+			Double   string
+			Float    string
+			Int32    string
+			Int64    string
+			Uint32   string
+			Uint64   string
+			Sint32   string
+			Sint64   string
+			Fixed32  string
+			Fixed64  string
+			Sfixed32 string
+			Sfixed64 string
+			Bool     string
+			String   string
+			Bytes    string
+		}
+	}
 }
 
 func (cfg *Config) IsExcluded(name string) bool {
