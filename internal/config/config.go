530339af7788f8297d784a57ca874990e592913a
balesz <balint.bode@gmail.com>
2022-04-11 21:41:42 +0200

    Add singleton to config package

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,16 +3,21 @@ package config
 import (
 	"errors"
 	"fmt"
+	"regexp"
+	"strings"
 
 	"github.com/balesz/protoc-gen-tmpl/internal/log"
 	"github.com/spf13/viper"
 )
 
-var ErrorReadFile = errors.New("can't read file")
+var (
+	ErrorReadFile    = errors.New("can't read file")
+	ErrorInvalidFile = errors.New("the file has wrong format")
+)
 
-var ErrorInvalidFile = errors.New("the file has wrong format")
+var cfg config
 
-func Load(path string) (*Config, error) {
+func Load(path string) (*config, error) {
 	v := viper.New()
 	v.AddConfigPath(path)
 	v.SetConfigName("protoc-gen-tmpl")
@@ -23,7 +28,6 @@ func Load(path string) (*Config, error) {
 		return nil, fmt.Errorf("config.Load: %w", ErrorReadFile)
 	}
 
-	var cfg Config
 	if err := v.UnmarshalExact(&cfg); err != nil {
 		log.Error("viper.Unmarshal: %v", err)
 		return nil, fmt.Errorf("config.Load: %w", ErrorInvalidFile)
@@ -32,33 +36,57 @@ func Load(path string) (*Config, error) {
 	return &cfg, nil
 }
 
-type Config struct {
+func IsExcluded(name string) bool {
+	for _, pattern := range cfg.Exclude {
+		if regexp.MustCompile(pattern).MatchString(name) {
+			return true
+		}
+	}
+	return false
+}
+
+func OutputByName(name string) string {
+	for _, out := range cfg.Output {
+		if strings.HasSuffix(name, out.Name) {
+			return out.Path
+		}
+	}
+	return ""
+}
+
+func Types() types {
+	return cfg.Types
+}
+
+type config struct {
 	Exclude []string
 	Output  []struct {
 		Name string
 		Path string
 	}
-	Types struct {
-		Enum     string
-		Map      string
-		Message  string
-		Repeated string
-		Scalar   struct {
-			Double   string
-			Float    string
-			Int32    string
-			Int64    string
-			Uint32   string
-			Uint64   string
-			Sint32   string
-			Sint64   string
-			Fixed32  string
-			Fixed64  string
-			Sfixed32 string
-			Sfixed64 string
-			Bool     string
-			String   string
-			Bytes    string
-		}
+	Types types
+}
+
+type types struct {
+	Enum     string
+	Map      string
+	Message  string
+	Repeated string
+	Scalar   struct {
+		Double   string
+		Float    string
+		Int32    string
+		Int64    string
+		Uint32   string
+		Uint64   string
+		Sint32   string
+		Sint64   string
+		Fixed32  string
+		Fixed64  string
+		Sfixed32 string
+		Sfixed64 string
+		Bool     string
+		String   string
+		Bytes    string
 	}
 }
